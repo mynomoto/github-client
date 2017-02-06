c0066dbeb956d264bd306f197378d402be317be8
mynomoto <mynomoto@gmail.com>
2017-02-06 08:52:16 -0200

    Dashboard namespace

---
+++ src/github_client/page/dashboard.cljs
@@ -0,0 +1,5 @@
+(ns github-client.page.dashboard
+  (:require
+    [hoplon.core :as h :refer [defelem case-tpl cond-tpl for-tpl if-tpl when-tpl]]
+    [hoplon.spectre-css :as s]
+    [javelin.core :as j :refer [cell] :refer-macros [cell= defc defc=]]))
